1cf07766c9c28906f8fccf697fb118fd3a3ba1f4
Eric <ericfeldman93@gmail.com>
2016-03-18 21:44:10 +0200

    remove moduels

--- client/typings/browser.d.ts
+++ client/typings/browser.d.ts
@@ -1,5 +1,2 @@
 /// <reference path="browser\ambient\es6-shim\es6-shim.d.ts" />
 /// <reference path="browser\ambient\netchart\netchart.d.ts" />
-/// <reference path="browser\ambient\zoomcharts\zoomcharts.d.ts" />
-/// <reference path="browser\definitions\netchart\netchart.d.ts" />
-/// <reference path="browser\definitions\zoomcharts\zoomcharts.d.ts" />
--- client/typings/main.d.ts
+++ client/typings/main.d.ts
@@ -1,5 +1,2 @@
 /// <reference path="main\ambient\es6-shim\es6-shim.d.ts" />
 /// <reference path="main\ambient\netchart\netchart.d.ts" />
-/// <reference path="main\ambient\zoomcharts\zoomcharts.d.ts" />
-/// <reference path="main\definitions\netchart\netchart.d.ts" />
-/// <reference path="main\definitions\zoomcharts\zoomcharts.d.ts" />
